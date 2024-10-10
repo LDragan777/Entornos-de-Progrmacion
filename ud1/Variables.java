3eec4b5655db85748e4f337de6ba219ecd680a09
LDragan777 <ldragan@iesch.org>
2024-10-10 17:10:22 +0200

    first commit

---
+++ ud1/Variables.java
@@ -0,0 +1,13 @@
+package ud1;
+
+public class Variables {
+
+    public static void main(Sting[] args) {
+        int a = 7;
+        int b = 2;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
